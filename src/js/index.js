502754cd443e8433ebc671e3ffb6fe52eab4c3b7
Mario Serrano <maseflores79@gmail.com>
2019-01-15 11:30:57 -0800

    construyendo el modelo de busqueda

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
+import Search from "./models/search";
 
-async function getResults(query) {
-  
-    // Usen su propia key cabroncitos >=) ...
-    const key = '76c2ca6f0e02fdfbf7550511a07b27fe'
-    try {
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
+const search = new Search('pizza');
 
-getResults('tomato pasta');
+search.getResults();
---
+++ src/js/models/search.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        // Usen su propia key cabroncitos >=) ...
+        const key = '76c2ca6f0e02fdfbf7550511a07b27fe'
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
