e0c99b354f42603289aa3d8534e65bc162eba675
Mario Serrano <maseflores79@gmail.com>
2019-01-15 10:06:08 -0800

    instalando axios y llamando al api de food2fork

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
-// Global app controller
-import num from './test';
+import axios from 'axios';
 
-const x = 23;
-console.log(`I imported ${num} from another module called test.`);
-console.log(`Variable x is ${x}.`);
+async function getResults(query) {
+  
+    // Usen su propia key cabroncitos >=) ...
+    const key = '76c2ca6f0e02fdfbf7550511a07b27fe'
+    try {
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+getResults('tomato pasta');
