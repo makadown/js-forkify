3a62d1542125f095dac35cf162ccd81d62239fcc
Mario Serrano <maseflores79@gmail.com>
2019-01-15 06:29:04 -0800

    instalando y configurando babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['@babel/polyfill', './src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'js/bundle.js'
@@ -15,5 +16,10 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+          ]
+    }
 };
